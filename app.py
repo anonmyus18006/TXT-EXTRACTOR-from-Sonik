f45fc00db77daad6da9cc86db5ffea4ceadb6559
sonic kuwal <158008154+Soninkuwal@users.noreply.github.com>
2026-05-20 21:12:14 +0530

    Refactor Flask app to use threading for execution

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from threading import Thread
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from spidy'
+@app.route("/")
+def home():
+    return "Bot Running"
 
+def run():
+    app.run(host="0.0.0.0", port=8080)
 
-if __name__ == "__main__":
-    app.run()
-  
+Thread(target=run).start()
